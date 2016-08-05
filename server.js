cba871711beb21d820e53f7c73f56af5f31a67ed
Kat Marchán <kzm@sykosomatic.org>
2016-08-04 22:39:10 -0700

    cleaned up server.js a bit and moved scopes out

--- server.js
+++ server.js
@@ -5,24 +5,27 @@ const event = require('./lib/event')
 const oauth = require('./lib/oauth')
 const bot = require('./lib/bot')
 
-bot.listen()
-
 const server = restify.createServer()
 
 server.use(restify.bodyParser())
-// server.use(function (req, res, next) {
-//   if (!req.body ||
-//       !req.body.token ||
-//       req.body.token !== process.env.VERIFICATION_TOKEN) {
-//     res.send(403)
-//     next(new Error('bad request token'))
-//   } else {
-//     next()
-//   }
-// })
 
-server.get('/', function (req, res, next) {
-  const body = `<a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot,channels:history,channels:read,emoji:read,groups:read,groups:history,reactions:read,reactions:write,users:read,chat:write:bot&client_id=64894231568.65752863537"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
+const scopes = [
+  'incoming-webhook',
+  'commands',
+  'admin',
+  'bot',
+  'channels:history',
+  'channels:read',
+  'emoji:read',
+  'groups:read',
+  'reactions:read',
+  'reactions:write',
+  'users:read',
+  'chat:write:user'
+]
+
+server.get('/install', function (req, res, next) {
+  const body = `<a href="https://slack.com/oauth/authorize?scope=${scopes}&client_id=64894231568.65752863537"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
   res.writeHead(200, {
     'Content-Length': Buffer.byteLength(body),
     'Content-Type': 'text/html'
@@ -36,4 +39,5 @@ server.post('/admin', admin)
 server.post('/event', event)
 server.get('/oauth', oauth)
 
+bot.listen()
 server.listen(process.env.PORT)
