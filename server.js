9610ff4e1bef6cdb89fe01f9f5347cdc36eaeabd
Kat Marchán <kzm@sykosomatic.org>
2016-10-15 21:53:19 -0400

    use fat arrows

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const scopes = [
   'chat:write:user'
 ]
 
-server.get('/install', function (req, res, next) {
+server.get('/install', (req, res, next) => {
   const body = `<a href="https://slack.com/oauth/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=${scopes}"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
   res.writeHead(200, {
     'Content-Length': Buffer.byteLength(body),
