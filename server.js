a4a880f21aff9f143d02515fd880061f038fedfa
Kat Marchán <kzm@sykosomatic.org>
2016-10-16 10:27:00 -0700

    refactor resources, buttons, events, etc, into separate things, use yargs

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const restify = require('restify')
-const button = require('./lib/button')
-const admin = require('./lib/admin')
-const signup = require('./lib/signup')
-const event = require('./lib/event')
-const oauth = require('./lib/oauth')
-const bot = require('./lib/bot')
-const installInviter = require('./lib/install-inviter')
+
+const command = require('./lib/res/command')
+const install = require('./lib/res/install')
+const button = require('./lib/res/button')
+const signup = require('./lib/res/signup')
+const event = require('./lib/res/event')
+const oauth = require('./lib/res/oauth')
+const installInviter = require('./lib/res/install-inviter')
+
+require('./lib/bot').listen()
 
 const server = restify.createServer()
-
-server.use(restify.bodyParser())
 
 const scopes = [
   'incoming-webhook',
@@ -18,22 +19,16 @@ const scopes = [
   'chat:write:user'
 ]
 
-server.get('/install', (req, res, next) => {
-  const body = `<a href="https://slack.com/oauth/authorize?client_id=${process.env.SLACK_CLIENT_ID}&scope=${scopes}"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`
-  res.writeHead(200, {
-    'Content-Length': Buffer.byteLength(body),
-    'Content-Type': 'text/html'
-  })
-  res.write(body)
-  res.end()
-  next()
-})
-server.post('/button', button)
-server.post('/admin', admin)
-server.post('/signup', signup)
-server.post('/event', event)
-server.get('/oauth', oauth)
-server.get('/install-inviter', installInviter)
+server.use(restify.bodyParser())
+const resources = [
+  command('/command'),
+  install('/install', scopes),
+  button('/button'),
+  signup('/signup'),
+  event('/event'),
+  oauth('/oauth'),
+  installInviter('/install-inviter')
+]
+resources.map(r => r(server))
 
-bot.listen()
 server.listen(process.env.PORT)
